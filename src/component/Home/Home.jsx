afd77dc29864c199f429fe0b6cbb1965c7517acf
angDecoder <angshudas012@gmail.com>
2023-07-04 15:09:30 +0530

    feat : home page created

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,25 +1,181 @@
 import React from 'react';
 import './Home.css';
+import logo from '../../assets/logo.svg'
+import scene from '../../assets/user.png';
+import Upvote from '../../assets/Upvote';
+import Bookmark from '../../assets/Bookmark';
+import Share from '../../assets/Share';
+
 
 function Home() {
   return (
     <div id='Home'>
-      <h1>Lorem ipsum dolor sit amet. In Return Is Shocking</h1>
-      <h2>Heading 2</h2>
-      <h3>Heading 3</h3>
-      <h4>heading 4</h4>
-      <p>paragraph - Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, consequatur nihil optio est a vel quia repellendus illo quod cum?</p>
-      
-      <ul>
-        <li>list 1</li>
-        <li>list 2</li>
-        <li>list 3</li>
-        <li>list 4</li>
-        <li>list 5</li>
-      </ul>
-      <h1>INSIGHTS</h1>
-      <button data-btn='green'>Click Me</button>
-      <button data-btn='red'>Click Me</button>
+      <section id='home-intro'>
+        <h1>Unraveling the Layers <br /> of Knowledge</h1>
+        <p>Discover stories, thinking, and <br /> expertise from writers on any topic.</p>
+        <button data-btn='black'>Get Started</button>
+        <img src={logo} alt="" />
+      </section>
+
+      <section id='home-postlist'>
+        <h1>Top Posts</h1>
+        <div className='post-card'>
+          <div className='card-creator'>
+              <img src={scene} alt="" />
+              <p>Angshu</p>
+              <h4>Read</h4>
+          </div>
+          <img src={scene} alt="" />
+          <div className='card-content'>
+            <h4>This the heading</h4>
+            <p>description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quis quae aut fugiat nemo repudiandae.</p>
+          </div>
+          <div className='card-bar'>
+            <Upvote votes={10} />
+            <Bookmark />
+            <Share />
+          </div>
+        </div>
+        <div className='post-card'>
+          <div className='card-creator'>
+              <img src={scene} alt="" />
+              <p>Angshu</p>
+              <h4>Read</h4>
+          </div>
+          <img src={scene} alt="" />
+          <div className='card-content'>
+            <h4>This the heading</h4>
+            <p>description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quis quae aut fugiat nemo repudiandae.</p>
+          </div>
+          <div className='card-bar'>
+            <Upvote votes={10} />
+            <Bookmark />
+            <Share />
+          </div>
+        </div>
+        <div className='post-card'>
+          <div className='card-creator'>
+              <img src={scene} alt="" />
+              <p>Angshu</p>
+              <h4>Read</h4>
+          </div>
+          <img src={scene} alt="" />
+          <div className='card-content'>
+            <h4>This the heading</h4>
+            <p>description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quis quae aut fugiat nemo repudiandae.</p>
+          </div>
+          <div className='card-bar'>
+            <Upvote votes={10} />
+            <Bookmark />
+            <Share />
+          </div>
+        </div>
+        <div className='post-card'>
+          <div className='card-creator'>
+              <img src={scene} alt="" />
+              <p>Angshu</p>
+              <h4>Read</h4>
+          </div>
+          <img src={scene} alt="" />
+          <div className='card-content'>
+            <h4>This the heading</h4>
+            <p>description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quis quae aut fugiat nemo repudiandae.</p>
+          </div>
+          <div className='card-bar'>
+            <Upvote votes={10} />
+            <Bookmark />
+            <Share />
+          </div>
+        </div>
+        <div className='post-card'>
+          <div className='card-creator'>
+              <img src={scene} alt="" />
+              <p>Angshu</p>
+              <h4>Read</h4>
+          </div>
+          <img src={scene} alt="" />
+          <div className='card-content'>
+            <h4>This the heading</h4>
+            <p>description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quis quae aut fugiat nemo repudiandae.</p>
+          </div>
+          <div className='card-bar'>
+            <Upvote votes={10} />
+            <Bookmark />
+            <Share />
+          </div>
+        </div>
+        <div className='post-card'>
+          <div className='card-creator'>
+              <img src={scene} alt="" />
+              <p>Angshu</p>
+              <h4>Read</h4>
+          </div>
+          <img src={scene} alt="" />
+          <div className='card-content'>
+            <h4>This the heading</h4>
+            <p>description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quis quae aut fugiat nemo repudiandae.</p>
+          </div>
+          <div className='card-bar'>
+            <Upvote votes={10} />
+            <Bookmark />
+            <Share />
+          </div>
+        </div>
+        <div className='post-card'>
+          <div className='card-creator'>
+              <img src={scene} alt="" />
+              <p>Angshu</p>
+              <h4>Read</h4>
+          </div>
+          <img src={scene} alt="" />
+          <div className='card-content'>
+            <h4>This the heading</h4>
+            <p>description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quis quae aut fugiat nemo repudiandae.</p>
+          </div>
+          <div className='card-bar'>
+            <Upvote votes={10} />
+            <Bookmark />
+            <Share />
+          </div>
+        </div>
+        <div className='post-card'>
+          <div className='card-creator'>
+              <img src={scene} alt="" />
+              <p>Angshu</p>
+              <h4>Read</h4>
+          </div>
+          <img src={scene} alt="" />
+          <div className='card-content'>
+            <h4>This the heading</h4>
+            <p>description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quis quae aut fugiat nemo repudiandae.</p>
+          </div>
+          <div className='card-bar'>
+            <Upvote votes={10} />
+            <Bookmark />
+            <Share />
+          </div>
+        </div>
+        <div className='post-card'>
+          <div className='card-creator'>
+              <img src={scene} alt="" />
+              <p>Angshu</p>
+              <h4>Read</h4>
+          </div>
+          <img src={scene} alt="" />
+          <div className='card-content'>
+            <h4>This the heading</h4>
+            <p>description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quis quae aut fugiat nemo repudiandae.</p>
+          </div>
+          <div className='card-bar'>
+            <Upvote votes={10} />
+            <Bookmark />
+            <Share />
+          </div>
+        </div>
+        
+        
+      </section>
+
     </div>
   )
 }
