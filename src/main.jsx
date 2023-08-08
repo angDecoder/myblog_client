5b2380c5b1d5b562282320673dfd79b5868db404
Angshu Das <angshudas012@gmail.com>
2023-08-08 10:43:47 +0530

    update : main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import store from './app/store.js';
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
   ,
 )
